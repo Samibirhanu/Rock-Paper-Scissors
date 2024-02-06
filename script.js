dec6c558c1cc2633db87a1926cd4db86f4d9c4f4
samuel <samuelberhanu75@gmail.com>
2024-02-06 16:00:13 +0300

    create variable for rock, paper and scissor
    to return the values when the function invoked

--- script.js
+++ script.js
@@ -2,8 +2,11 @@
 function getComputerChoice(){
     // this will give us random number 0 to 100
     const random = Math.floor(Math.random() * 100);
+    // create variable for rock paper scissor
+    const rock = "Rock";
+    const paper = "Paper";
+    const scissor = "scissor";
     
-        
 }
 
 
