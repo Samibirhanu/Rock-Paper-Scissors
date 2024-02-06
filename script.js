2b4a9b02720d553992f05279d671aca17e9f5abb
samuel <samuelberhanu75@gmail.com>
2024-02-06 16:01:10 +0300

    Create if statment
    that return rock if 1 - 30, paper  31 - 60, scissor  61 to 90

--- script.js
+++ script.js
@@ -6,7 +6,16 @@ function getComputerChoice(){
     const rock = "Rock";
     const paper = "Paper";
     const scissor = "scissor";
-    
+    // return rock if 1 - 30, paper  31 - 60, scissor  61 to 90 with if statment
+    if (random <= 30){
+        return rock;
+    } 
+    else if(random > 30 && random <= 60){
+        return paper;
+    }
+    else
+        return scissor;
+        
 }
 
 
