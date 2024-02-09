ccd83ffa001629ed6e3910d08d947b704ab996b5
samuel <samuelberhanu75@gmail.com>
2024-02-09 10:18:16 +0300

    Create a function that plays a single round of the game
    has two parameters one for computer and the other for user choice and compare it with if stament and return who wins

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function getComputerChoice(){
     const random = Math.floor(Math.random() * 3);
     // create variable for rock paper scissor
   
-    const rock = "Rock";
+    const rock = "rock";
     const paper = "Paper";
     const scissor = "scissor";
     // return rock if 0, paper 1, scissor 2 with if statment
@@ -29,9 +29,26 @@ function playerSelection(){
     if (userInput == "rock" || userInput == "paper" || userInput == "scissor"){
         return userInput;
     } else
-        console.log("The user input can be only rock paper and scissors !!");
+        return console.log("The user input can be only rock paper and scissor !!");
 }
 
-
+// single round play of computer and user 
+//create a function that plays single round that taks two parameters
+function singleRoundPlay(userSelection, computerSelection){
+    // asign the parameters to computer choise and player selection function 
+    userSelection = playerSelection();
+    computerSelection = getComputerChoice();
+    // check the parametrs 
+    console.log(`computer is ${computerSelection} | you are ${userSelection}`);
+    // create if statment to compare the gameplay
+    if(((userSelection == "paper" && computerSelection == "rock") || (userSelection == "scissor" && computerSelection == "paper" ) || (userSelection == "rock" && computerSelection == "scissor"))){
+        console.log("user wins!");
+    } else if ((userSelection == "rock" && computerSelection == "paper") || (userSelection == "paper" && computerSelection == "scissor" ) || (userSelection == "scissor" && computerSelection == "rock")){
+        console.log("Computer wins! ");
+    } else 
+        console.log("its a tie");
+    
+}
+singleRoundPlay();
 
 
